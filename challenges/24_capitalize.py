32c86e4f0320cefa6f7e3610f443cc63b2dfedf5
Mark <56827081+MarkVoitov@users.noreply.github.com>
2022-06-06 08:58:57 +0300

    Create 24_capitalize.py

---
+++ challenges/24_capitalize.py
@@ -0,0 +1,15 @@
+def solve(s):
+    capitalized_string = ' '.join(map(str.capitalize, s.split(' ')))
+    return capitalized_string
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
